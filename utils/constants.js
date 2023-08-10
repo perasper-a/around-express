a51f4182e6bad5a49e65336f6ca0f937faafa4b2
perasper-a <allybakh@outlook.com>
2023-08-10 22:48:17 +0300

    12th fix 2

--- utils/constants.js
+++ utils/constants.js
@@ -1,10 +1,9 @@
-// constants.js
 const BAD_REQUEST = 400;
 const NOT_FOUND = 404;
 const DEFAULT = 500;
 
 module.exports = {
-    BAD_REQUEST,
-    NOT_FOUND,
-    DEFAULT,
+  BAD_REQUEST,
+  NOT_FOUND,
+  DEFAULT,
 };
